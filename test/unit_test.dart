05c0c4a5c1bd0470b948c9b710fbe9e4b5c39c67
prfigueiredo <paulofigueiredo@id.uff.br>
2023-03-30 20:02:08 -0300

    Update unit_test

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -1,8 +1,9 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'dart:math';
 
 void main() {
   group('Testing currency converter', () { 
-    test('BRL currency to USD/EUR', () {
+    test('BRL currency to USD/EUR - positive values', () {
       //setup
       double? reais = 5000.00;
       double? euros = 892.62;
@@ -13,6 +14,18 @@ void main() {
       //test
       expect(outputUSD, closeTo(5.16, 0.01));
       expect(outputEUR, closeTo(5.60, 0.01));
+    });
+    test('BRL currency to USD/EUR - negative values', () {
+      //setup
+      double? reais = -12;
+      double? euros = -2.16;
+      double? dolares = -2.36;
+      //do
+      double? outputUSD = reais/dolares;
+      double? outputEUR = reais/euros;
+      //test
+      expect(outputUSD, closeTo(5.08, 0.01));
+      expect(outputEUR, closeTo(5.55, 0.01));
     });
   });
 }
